723d92fc9e0ad8ab2a30239b3c7e90943a3ce7b0
V Mahesh Kumar <maheshkumarvmk0@gmail.com>
2024-01-21 15:06:13 +0530

    Created Todo list GUI

--- main.py
+++ main.py
@@ -4,77 +4,59 @@ import pickle
 def create_file():
     file_pointer = open("data.bin", 'wb')
     pickle.dump([], file_pointer)
-    pickle.dump([], file_pointer)
     file_pointer.close()
 
 def gettasks():
     f = open("data.bin", 'rb')
     tasks = pickle.load(f)
-    completed = pickle.load(f)
     f.close()
-    return tasks, completed
+    return tasks
 
-def puttasks(tasks, completed):
+def puttasks(tasks):
     f = open("data.bin", 'wb')
     pickle.dump(tasks, f)
-    pickle.dump(completed, f)
     f.close()
 
 def printtasks():
-    tasks, completed = gettasks()
+    tasks = gettasks()
     for i, task in enumerate(tasks, start=1):
         print(f"{i}.\t{task}")
 
 def addtask():
-    tasks, completed = gettasks()
+    tasks = gettasks()
     task = input("Enter the task : ")
     tasks.append(task)
-    puttasks(tasks, completed)
-
-def completetask():
-    tasks, completed = gettasks()
-    printtasks()
-    choice = int(input("Enter the task number : "))
-    if 0<=choice<len(choice):
-        task = tasks.pop(choice-1)
-        completed.append(task)
-        puttasks(tasks, completed)
-    else:
-        print("Invalid Choice")
+    puttasks(tasks)
 
 def deletetask():
     printtasks()
+    tasks = gettasks()
     choice = int(input("Enter the task number : "))
-    if 0<=choice<len(choice):
+    if 0<=choice-1<len(tasks):
         tasks.pop(choice-1)
-        puttasks(tasks, completed)
+        puttasks(tasks)
     else:
         print("Invalid Choice")
-
-def showcompletedtasks():
-    tasks, completed = gettasks()
-    for i, task in enumerate(completed, start=1):
-        print(f"{i}.\t{task}")
 
 if __name__ == "__main__":
     if not os.path.isfile("data.bin"):
         create_file()
     while True:
-        print("1. Add Task")
-        print("2. Complete Task")
+        print("1. Show Tasks")
+        print("2. Add Task")
         print("3. Delete Task")
-        print("4. Show completed tasks")
-        print("5. Exit")
-        choice = int(input("Enter your choice : "))
-        if choice==1:
-            addtask()
-        elif choice==2:
-            completetask()
-        elif choice==3:
-            deletetask()
-        elif choice==4:
-            showcompletedtasks()
-        elif choice==5:
-            break
-        else:
-            print("Invalid chioce")
+        print("4. Exit")
+        try:
+            choice = int(input("Enter your choice : "))
+            if choice==1:
+                printtasks()
+            elif choice==2:
+                addtask()
+            elif choice==3:
+                deletetask()
+            elif choice==4:
+                break
+            else:
+                print("Invalid chioce")
+        except:
+            print("Error occured please try again")
